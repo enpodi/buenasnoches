3c9fdb805ab2c484ff81ce6136a83cd959f9f623
Enrique Pomposo Diaz <enrique.pomposo@globant.com>
2022-04-12 16:10:44 -0500

    new sprongboot gradle project added

---
+++ artBuenasNoches/src/main/java/com/example/artBuenasNoches/NameBuenasNochesApplication.java
@@ -0,0 +1,13 @@
+package com.example.artBuenasNoches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameBuenasNochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameBuenasNochesApplication.class, args);
+	}
+
+}
